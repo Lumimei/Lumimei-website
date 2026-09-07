861ee92c241ec651339e559246b185efc117873a
Lumimei <lumimeicambodia@gmail.com>
2026-09-07 15:02:42 +0700

    Add translations for multiple languages

---
+++ src/data/translation.ts
@@ -0,0 +1,14 @@
+export const translations = {
+  km: {
+    welcome: "ស្វាគមន៍មកកាន់ Lumimei",
+    subtitle: "អ្នកជំនាញថែរក្សាស្បែកមុខគ្រប់ប្រភេទ",
+  },
+  zh: {
+    welcome: "欢迎来到 Lumimei",
+    subtitle: "各类肌肤护理专家",
+  },
+  en: {
+    welcome: "Welcome to Lumimei",
+    subtitle: "Expert Skincare Solutions",
+  },
+};
